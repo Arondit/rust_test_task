1d7a6594b62955b3898f82d3c0bbe88ee5439bd2
Arondit <57594292+Arondit@users.noreply.github.com>
2022-09-07 21:14:29 +0300

    fix point_neighbours, add tests

--- main.rs
+++ main.rs
@@ -23,22 +23,10 @@ fn point_can_be_visited(point: Point) -> bool {
 ///Возвращает четырех "соседей" точки
 fn point_neighbours(point: Point) -> [Point; 4] {
     return [
-        Point{
-            x_coordinate: point.x_coordinate + 1, 
-            y_coordinate: point.y_coordinate + 1,
-        },
-        Point{
-            x_coordinate: point.x_coordinate + 1, 
-            y_coordinate: point.y_coordinate - 1,
-        },
-        Point{
-            x_coordinate: point.x_coordinate - 1, 
-            y_coordinate: point.y_coordinate + 1,
-        },
-        Point{
-            x_coordinate: point.x_coordinate - 1, 
-            y_coordinate: point.y_coordinate - 1,
-        },
+        Point{x_coordinate: point.x_coordinate + 1, y_coordinate: point.y_coordinate},
+        Point{x_coordinate: point.x_coordinate - 1, y_coordinate: point.y_coordinate},
+        Point{x_coordinate: point.x_coordinate, y_coordinate: point.y_coordinate + 1},
+        Point{x_coordinate: point.x_coordinate, y_coordinate: point.y_coordinate - 1},
     ];
 }
 
@@ -84,7 +72,7 @@ fn find_count_of_visited(start_point: Point) -> i32 {
     return visited.len().try_into().unwrap();
 }
 
- 
+
 fn main() {
     let start_point = Point{
         x_coordinate: 1000,
@@ -99,4 +87,69 @@ fn main() {
         start_point.x_coordinate,
         start_point.y_coordinate,
     );
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    ///Arrange: Точка с координатами, сумма цифр которых равна 26
+    ///Act: Для этой точки вызывается point_can_be_visited
+    ///Assert: Точка не может быть посещена, так как сумма больше 25
+    fn test_can_not_visit_point_26() {
+        let point = crate::Point{x_coordinate: 8882, y_coordinate: 0};
+
+        assert_eq!(crate::point_can_be_visited(point), false);
+    }
+
+    #[test]
+    ///Arrange: Точка с координатами, сумма цифр которых равна 25
+    ///Act: Для этой точки вызывается point_can_be_visited
+    ///Assert: Точка не может быть посещена, так как сумма не больше 25
+    fn test_can_visit_point_25() {
+        let point = crate::Point{x_coordinate: 8881, y_coordinate: 0};
+
+        assert_eq!(crate::point_can_be_visited(point), true);
+    }
+
+    #[test]
+    ///Arrange: Точка с координатами, сумма цифр которых равна 24
+    ///Act: Для этой точки вызывается point_can_be_visited
+    ///Assert: Точка не может быть посещена, так как сумма меньше 25
+    fn test_can_visit_point_24() {
+        let point = crate::Point{x_coordinate: 888, y_coordinate: 0};
+
+        assert_eq!(crate::point_can_be_visited(point), true);
+    }
+
+    #[test]
+    ///Arrange: Точка в середине сетки координат
+    ///Act: Для этой точки вызывается point_neighbours
+    ///Assert: Соседи точки - точки слева, справа, сверху, снизу
+    fn test_get_neighbours() {
+        let point = crate::Point{x_coordinate: 0, y_coordinate: 0};
+
+        let expected_neighbours = [
+            crate::Point{x_coordinate: 1, y_coordinate: 0},
+            crate::Point{x_coordinate: -1, y_coordinate: 0},
+            crate::Point{x_coordinate: 0, y_coordinate: 1},
+            crate::Point{x_coordinate: 0, y_coordinate: -1},
+        ];
+
+        assert_eq!(crate::point_neighbours(point), expected_neighbours);
+    }
+
+    #[test]
+    ///Arrange: Точка с координатами (1000, 1000)
+    ///Act: Для этой точки вызывается find_count_of_visited
+    ///Assert: Из точки (1000, 1000) муравей может посетить 148848 клеток
+    fn test_visited_count_for_1000() {
+        let start_point = crate::Point{
+            x_coordinate: 1000,
+            y_coordinate: 1000,
+        };
+    
+        let count = crate::find_count_of_visited(start_point);
+
+        assert_eq!(count, 148848);
+    }
 }
